f9b8c01a4620900739e15293371c251b27e4ba2c
Oyindamola Oyetunmibi <oyindamola.oyetunmibi@omnipresent.com>
2023-02-06 16:59:21 +0100

    excercise 4

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,16 +1,6 @@
 import React from 'react';
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
-
-
-// Pick a random word on every pageload.
-const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
-
 function Game({ guess, setGuess, guesses, setGuesses }) {
-
   const handleGuess = (event) => {
     const guessValue = event.target.value;
     setGuess(guessValue.toUpperCase());
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
+
+import { sample } from '../../utils';
+import { WORDS } from '../../data';
+
+
+// Pick a random word on every pageload.
+const answer = sample(WORDS);
+// To make debugging easier, we'll log the solution in the console.
+console.info({ answer });
+
 
 function Guess({ value }) {
+  
+  const guessArr = checkGuess(value, answer);
+  
+  function getStyle(letterIndex) {
+    if(!value) {
+      return;
+    }
+    return guessArr[letterIndex].status;
+  }
+
 
   return (
     <p className="guess">
     {range(5).map(num => (
-      <span key={num} className="cell">
+      <span key={num} className={`cell ${getStyle(num)}`}>
         {value ? value[num] : undefined}
       </span>
     ))}
